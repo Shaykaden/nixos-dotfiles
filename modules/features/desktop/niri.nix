133c1204730752bbb61289a7cfa7a2a41a131d60
Shaykaden <none@none.com>
2026-08-23 15:37:09 +0200

    feat: add xdg portal configuration

--- modules/features/desktop/niri.nix
+++ modules/features/desktop/niri.nix
@@ -4,14 +4,34 @@
     self.homeModules.niri
   ];
 
-  flake.nixosModules.niri = { ... }: {
+  flake.nixosModules.niri = { pkgs, ... }: {
     imports = [ inputs.niri.nixosModules.niri ];
     programs.niri = {
       enable = true;
     };
+    xdg = {
+      portal = {
+        enable = true;
+        config.niri = {
+          default = ["gnome" "gtk"];
+          "org.freedesktop.impl.portal.Access" = "gtk";
+          "org.freedesktop.impl.portal.FileChooser" = "gtk";
+          "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+          "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+        };
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gnome
+          xdg-desktop-portal-gtk
+        ];
+      };
+    };
   };
 
-  flake.homeModules.niri = { ... }: {
+  flake.homeModules.niri = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      xwayland-satellite # xwayland support
+    ];
+
     programs.niri.settings = {
       layout.gaps = 16;
 
