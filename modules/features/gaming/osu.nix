9a73e7103f32a50854275acfa0e91677289d3df0
Shaykaden <none@none.com>
2026-09-03 21:41:54 +0200

    feat: add osu-lazer + opentabletdriver

---
+++ modules/features/gaming/osu.nix
@@ -0,0 +1,19 @@
+{ self, ... }: {
+  flake.homeModules.phosConfiguration.imports = [
+    self.homeModules.osu
+  ];
+
+  flake.nixosModules.phosConfiguration.imports = [
+    self.nixosModules.opentabletdriver
+  ];
+
+  flake.homeModules.osu = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      osu-lazer-bin
+    ];
+  };
+
+  flake.nixosModules.opentabletdriver = { ... }: {
+    hardware.opentabletdriver.enable = true;
+  };
+}
