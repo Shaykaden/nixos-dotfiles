0d28b97888360bf00eccc596a9987d05bcab304a
Shaykaden <none@none.com>
2026-09-06 22:59:35 +0200

    feat: add dolphin

---
+++ modules/features/desktop/file-manager.nix
@@ -0,0 +1,12 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.phosConfiguration.imports = [
+    self.nixosModules.dolphin
+  ];
+
+  flake.nixosModules.dolphin = { pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      kdePackages.dolphin # This is the actual dolphin package
+      kdePackages.qtsvg
+    ];
+  };
+}
