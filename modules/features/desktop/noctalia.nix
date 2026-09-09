36b3af7974672975709f5be778339c47261a88f0
Shaykaden <none@none.com>
2026-09-09 18:33:34 +0200

    feat: add noctalia-greetd

--- modules/features/desktop/noctalia.nix
+++ modules/features/desktop/noctalia.nix
@@ -4,13 +4,31 @@
     self.homeModules.noctalia
   ];
 
-  flake.homeModules.noctalia = { ... }: {
+  flake.nixosModules.phosConfiguration.imports = [
+    self.nixosModules.noctalia-greetd
+  ];
+
+  flake.homeModules.noctalia = { pkgs, ... }: {
 
     imports = [ inputs.noctalia.homeModules.default ];
 
     programs.noctalia = {
       enable = true;
       #settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
+    };
+  };
+
+  flake.nixosModules.noctalia-greetd = { pkgs, ... }: {
+    services.displayManager.noctalia-greeter = {
+      enable = true;
+      settings = {
+        cursor.size = 24;
+        keyboard.layout = "us";
+      };
+      cursorTheme = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+      };
     };
   };
 }
