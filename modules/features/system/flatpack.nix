f6504f62ab38fe3ee79ae4f8447a9bd17a0834f7
Shaykaden <none@none.com>
2026-09-13 14:24:41 +0200

    feat: update flatpack settings

--- modules/features/system/flatpack.nix
+++ modules/features/system/flatpack.nix
@@ -2,5 +2,15 @@
   flake.nixosModules.flatpack = { }: {
     # check modules
     services.flatpak.enable = true;
+    services.flatpak.update.auto = {
+      enable = true;
+      onCalendar = "weekly";
+    };
+    services.flatpak.uninstallUnmanaged = true;
+
+    # Exemple : install a flatpack package
+    #services.flatpak.packages = [
+    #  "im.riot.Riot"
+    #];
   };
 }
